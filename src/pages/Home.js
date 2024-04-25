65027e25356e724a02e4b3ae61089993d771e327
oladips772 <oladips200@gmail.com>
2024-04-25 12:20:04 +0100

    selecting media working perfectly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,51 @@
 /** @format */
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState, useRef } from "react";
 import Header from "../components/Header";
 
 function Home() {
-  const [number, setNumber] = useState(0);
+  const [fileChosen, setFileChosen] = useState(null);
+  const [imageChosen, setImageChosen] = useState("");
+  const [audioChosen, setAudioChosen] = useState("");
+  const [videoChosen, setVideoChosen] = useState("");
+  const inputRef = useRef();
 
-  const increase = () => {
-    setNumber((number) => (number = number + 1));
+  const preview = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setFileChosen(readerEvent.target.result);
+    };
   };
 
-  const decrease = () => {
-    setNumber((number) => (number = number - 1));
-  };
+  useEffect(() => {
+    if (fileChosen?.includes("image")) {
+      setImageChosen(fileChosen);
+    }
+    if (fileChosen?.includes("video")) {
+      setVideoChosen(fileChosen);
+    }
+    if (fileChosen?.includes("audio")) {
+      setAudioChosen(fileChosen);
+    }
+  }, [fileChosen]);
 
   return (
     <div>
-      <button onClick={decrease}>decrease</button>
-      <h1>{number}</h1>
-      <button onClick={increase}>increase</button>
+      <input type="file" onChange={(e) => preview(e)} hidden ref={inputRef} />
+      <button onClick={() => inputRef.current.click()}>Select Image</button>
+
+      {imageChosen && (
+        <img src={imageChosen} alt="" srcset="" className="image" />
+      )}
+
+      {videoChosen && (
+        <video src={videoChosen} controls height={300} width={400}></video>
+      )}
+      {audioChosen && (
+        <audio src={audioChosen} controls height={300} width={400}></audio>
+      )}
     </div>
   );
 }
